81ae4fa0eb733e1c2b82b6ca6ddf1206fa17185e
GeekyUV <singhyuvi973@gmail.com>
2024-05-31 12:28:30 +0530

    associations added

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :title, :state, :deadline, presence: true
+
+  STATES = ['Backlog', 'In-progress', 'Done']
+
+  validates :state, inclusion: { in: STATES }
+
 end
