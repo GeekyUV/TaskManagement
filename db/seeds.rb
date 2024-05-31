89eb6117597d2313028eb5ae776560f58b1792ec
GeekyUV <singhyuvi973@gmail.com>
2024-05-31 12:48:41 +0530

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user1 = User.create!(email: 'yuvraj@gmail.com', password: 'yuvraj')
+user2 = User.create!(email: 'amaha@example.com', password: 'amaha7777')
+
+user1.tasks.create!(title: 'Design landing page', description: 'Design a visually appealing landing page for the website', state: 'Backlog', deadline: Time.now + 5.days)
+user1.tasks.create!(title: 'Implement user authentication', description: 'Implement user authentication using Devise gem', state: 'In-progress', deadline: Time.now + 10.days)
+user1.tasks.create!(title: 'Write blog post', description: 'Write a blog post about the latest features of the product', state: 'Done', deadline: Time.now - 2.days)
+
+user2.tasks.create!(title: 'Fix bug in payment module', description: 'Investigate and fix a bug causing payment failures in the payment module', state: 'In-progress', deadline: Time.now + 3.days)
+user2.tasks.create!(title: 'Optimize database queries', description: 'Optimize slow database queries to improve application performance', state: 'Backlog', deadline: Time.now + 7.days)
+user2.tasks.create!(title: 'Prepare presentation for client meeting', description: 'Prepare a presentation to showcase project progress and future plans to the client', state: 'Backlog', deadline: Time.now + 2.days)
